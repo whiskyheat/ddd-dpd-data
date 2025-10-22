f924e7a45071e785ccf682412a864e10abc8c238
whiskyheat <whiskywheat@protonmail.com>
2025-10-22 13:09:48 +0200

    Tag: add fields

--- src/ddd.py
+++ src/ddd.py
@@ -3,4 +3,22 @@ from dataclasses import dataclass
 
 @dataclass
 class Tag:
-    pass
+    WochenID: int
+    FolgenID: int
+    Datum: str
+    Ort: str | None
+
+    K1: int | None
+    K2: int | None
+    K3: int | None
+    K4: int | None
+    K5: int | None
+
+    summe: int | None
+
+    mikkel: int | None
+    andi: int | None
+    chat: int | None
+
+    person: str
+    bemerkung: str
